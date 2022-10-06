bd8ff0835fb86e995c0409f0c57755f59584b4d1
Marc Rosell <36154632+MarcRosellH@users.noreply.github.com>
2022-10-06 10:00:41 +0200

    TCP/UDP setup

---
+++ Handball Fu/Assets/Scripts/Server/UDPnet.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+public class UDPnet : MonoBehaviour
+{
+    Socket newSocket;
+    IPEndPoint ipep;
+    IPEndPoint sender;
+
+    Thread threadConnect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Socket creation
+        newSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        
+        ipep = new IPEndPoint(IPAddress.Any, 9999);
+        threadConnect = new Thread(ThreadNetConnect);
+        
+        sender = new IPEndPoint
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ThreadNetConnect()
+    {
+        newSocket.Bind(ipep);
+        Debug.Log("Waiting for a client...");
+    }
+}
