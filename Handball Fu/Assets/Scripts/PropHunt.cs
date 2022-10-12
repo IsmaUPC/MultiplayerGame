d580b230a0eafae56ffd996f4940cd3bb2d0d987
Alejandro Moreno <jandro.ms01@gmail.com>
2022-10-12 10:14:09 +0200

    Prop Hunt change material and disable cosmetics

--- Handball Fu/Assets/Scripts/PropHunt.cs
+++ Handball Fu/Assets/Scripts/PropHunt.cs
@@ -6,16 +6,22 @@ using UnityEngine.InputSystem;
 public class PropHunt : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] private Mesh[] newModels;
     [SerializeField] private SkinnedMeshRenderer meshFilter;
+    [SerializeField] private Mesh[] newModels;
+    [SerializeField] private Material[] newMaterials;
+    private GameObject[] bodyParts;
+
     public float timeToConvert = 2;
     private Mesh originalMesh;
-
+    private Material originalMaterial;
     public bool iAmTransformed = false;
+
     //[SerializeField] private PlayerController controller;
     void Start()
     {
         originalMesh = meshFilter.sharedMesh;
+        originalMaterial = meshFilter.sharedMaterial;
+        bodyParts = GetComponent<PlayerData>().bodyParts;
     }
 
     // Update is called once per frame
@@ -27,13 +33,30 @@ public class PropHunt : MonoBehaviour
     public void ChangeMesh()
     {
         if(!iAmTransformed)
-            meshFilter.sharedMesh = newModels[Random.Range(0, newModels.Length)];
+        {
+            int index = Random.Range(0, newModels.Length);
+            meshFilter.sharedMesh = newModels[index];
+            meshFilter.sharedMaterial = newMaterials[index];
+
+            for (int i = 0; i < bodyParts.Length; i++)
+            {
+                bodyParts[i].SetActive(false);
+            }
+        }
         iAmTransformed = true;
     }
     public void ResetMesh()
     {
         if (iAmTransformed)
+        {
             meshFilter.sharedMesh = originalMesh;
+            meshFilter.sharedMaterial = originalMaterial;
+
+            for (int i = 0; i < bodyParts.Length; i++)
+            {
+                bodyParts[i].SetActive(true);
+            }
+        }
         iAmTransformed = false;
     }
 }
