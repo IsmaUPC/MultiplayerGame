1c7f95ca4b57774a825016b15784ce547c1dee29
Marc Rosell <36154632+MarcRosellH@users.noreply.github.com>
2022-10-22 13:44:52 +0200

    Almost processing all data in UDP

--- Handball Fu/Assets/Scripts/Server/UDP/UDPServer.cs
+++ Handball Fu/Assets/Scripts/Server/UDP/UDPServer.cs
@@ -17,6 +17,16 @@ using System.Threading;
  * - Send events and final decisions
  */
 
+/*
+ * For now, messages between servers and clients are structured like:
+ * characters in positions 0, 1 and 2 are the 3 last digits of an IP (the sender)
+ * character in position 3 is a letter defining the event type of this data
+ * 
+ * Data example: "135MHello mates!"
+ * 
+ * Later on, ip identifier and type can be replaced by using one "unsigned" byte for each one
+ */
+
 public class UDPServer : MonoBehaviour
 {
     // Event types
@@ -24,6 +34,7 @@ public class UDPServer : MonoBehaviour
     {
         EVENT_CONNECTION,       // A client wants to connect
         EVENT_DISCONNETION,     // A client wants to disconnect
+        EVENT_KEEPCONNECT,      // A client is still connected
         EVENT_MESSAGE,          // A client sent a message
         EVENT_UPDATE,           // A client sent an updated "transform"
     };
@@ -33,15 +44,15 @@ public class UDPServer : MonoBehaviour
     {
         public EVENT_TYPE type; // What kind of event is
         public string data;     // Event data itself
-        public IPEndPoint ep;   // Who sent it
+        public IPEndPoint ipep;   // Who sent it
     }
 
     // Event list to process
     private Queue<Event> eventQueue;
+    private Queue<Event> sendQueue;
 
-    // Accepting 5 clients a part of this
+    // Accepting 6 clients a part of this
     private ArrayList clientSockets = new ArrayList();
-    private EndPoint[] remotes = new EndPoint[5];
 
     // This will save basic data of clients, as self-given username, an ip end point and id
     // id is the last 3 ip digits
@@ -50,26 +61,46 @@ public class UDPServer : MonoBehaviour
         public string name;
         public IPEndPoint ipep;
         public int id;
+        public float lastContact;
     }
 
-    private ClientData[] clientsData = new ClientData[5];
+    private ClientData[] clientsData = new ClientData[6];
 
     // Total sockets *One socket is for initial connection*
     // After initial connection, the server send an unused port to the client
     private uint openPorts;
     private int initialPort;
 
+    struct Ports
+    {
+        public int port;
+        public bool isUsed;
+        public IPAddress remoteIP;
+    }
+
+    Ports[] ports = new Ports[6];
+
     // Server threads
     private Thread threadServerInBound;
     private Thread threadServerProcess;
     private Thread threadServerOutBound;
 
+    private object clientsLock = new object();
+    private object portsLock = new object();
+    private object eventQueueLock = new object();
+    private object sendQueueLock = new object();
+
+    // Host address
+    IPAddress host;
+
     // Start is called before the first frame update
     void Start()
     {
-        // Ports available from 9050 to 9055
+        GetHostIP();
+
+        // Ports available from 9050 to 9056
         initialPort = 9050;
-        openPorts = 6;
+        openPorts = 7;
 
         // Fill sockets list to get data
         for (int i = 0; i < openPorts; ++i)
@@ -79,11 +110,18 @@ public class UDPServer : MonoBehaviour
                         SocketType.Dgram, ProtocolType.Udp));
 
             // Bind each socket with a different port
-            ((Socket)clientSockets[i]).Bind(new IPEndPoint(IPAddress.Any, initialPort+i));
+            ((Socket)clientSockets[i]).Bind(new IPEndPoint(IPAddress.Any, initialPort + i));
+
+            if (initialPort + i > 9050)
+            {
+                ports[i - 1].port = initialPort + i;
+                ports[i - 1].isUsed = false;
+            }
         }
 
         // Initialize event queue
         eventQueue = new Queue<Event>();
+        sendQueue = new Queue<Event>();
     }
 
     public void OnServerClose()
@@ -94,7 +132,7 @@ public class UDPServer : MonoBehaviour
         if (threadServerOutBound.IsAlive) threadServerOutBound.Abort();
 
         // Close all sockets
-        for (int i = clientSockets.Count-1; i >= 0; --i)
+        for (int i = clientSockets.Count - 1; i >= 0; --i)
         {
             ((Socket)clientSockets[i]).Close();
         }
@@ -117,6 +155,7 @@ public class UDPServer : MonoBehaviour
             // If we have data to check do:
             for (int i = 0; i < rr.Count; ++i)
             {
+                // Get data
                 int recv;
                 byte[] data = new byte[1024];
                 string tmpMessage;
@@ -126,28 +165,63 @@ public class UDPServer : MonoBehaviour
 
                 recv = ((Socket)rr[i]).ReceiveFrom(data, ref remote);
                 tmpMessage = Encoding.ASCII.GetString(data, 0, recv);
-                Event e = new Event();
-                switch(tmpMessage[3])
+                Event e;
+
+                // Check what event type it is and save it to process
+                switch (tmpMessage[3])
                 {
                     case 'C':
+                        IPEndPoint ep = ((Socket)rr[i]).LocalEndPoint as IPEndPoint;
+                        if (ep.Port != 9050)
+                            break;
+                        e = new Event();
                         e.type = EVENT_TYPE.EVENT_CONNECTION;
                         e.data = tmpMessage;
-                        e.ep = (IPEndPoint)remote;
+                        e.ipep = remote as IPEndPoint;
+                        lock (eventQueueLock)
+                        {
+                            eventQueue.Enqueue(e);
+                        }
                         break;
                     case 'D':
+                        e = new Event();
                         e.type = EVENT_TYPE.EVENT_DISCONNETION;
                         e.data = tmpMessage;
-                        e.ep = (IPEndPoint)remote;
+                        e.ipep = remote as IPEndPoint;
+                        lock (eventQueueLock)
+                        {
+                            eventQueue.Enqueue(e);
+                        }
+                        break;
+                    case 'K':
+                        e = new Event();
+                        e.type = EVENT_TYPE.EVENT_KEEPCONNECT;
+                        e.data = tmpMessage;
+                        e.ipep = remote as IPEndPoint;
+                        lock (eventQueueLock)
+                        {
+                            eventQueue.Enqueue(e);
+                        }
                         break;
                     case 'M':
+                        e = new Event();
                         e.type = EVENT_TYPE.EVENT_MESSAGE;
                         e.data = tmpMessage;
-                        e.ep = (IPEndPoint)remote;
+                        e.ipep = remote as IPEndPoint;
+                        lock (eventQueueLock)
+                        {
+                            eventQueue.Enqueue(e);
+                        }
                         break;
                     case 'U':
+                        e = new Event();
                         e.type = EVENT_TYPE.EVENT_UPDATE;
                         e.data = tmpMessage;
-                        e.ep = (IPEndPoint)remote;
+                        e.ipep = remote as IPEndPoint;
+                        lock (eventQueueLock)
+                        {
+                            eventQueue.Enqueue(e);
+                        }
                         break;
                     default:
                         break;
@@ -159,51 +233,136 @@ public class UDPServer : MonoBehaviour
     // This thread is responsible to process all data
     private void ThreadServerProcess()
     {
+        while (true)
+        {
+            Queue<Event> events;
+            lock (eventQueueLock)
+            {
+                events = eventQueue;
+            }
+            while (events.Count > 0)
+            {
+                ClientData[] clients;
+                Ports[] prts;
+                Event e = events.Dequeue();
+                lock (clientsLock)
+                {
+                    clients = clientsData;
+                }
+                lock(portsLock)
+                {
+                    prts = ports;
+                }
+                switch (e.type)
+                {
+                    case EVENT_TYPE.EVENT_DISCONNETION:
 
+                        bool disconnected = false;
+                        for (int i = 0; i < prts.Length; ++i)
+                        {
+                            if (prts[i].remoteIP == e.ipep.Address)
+                            {
+                                lock (portsLock)
+                                {
+                                    ports[i].isUsed = false;
+                                    ports[i].remoteIP = IPAddress.Any;
+                                }
+                                disconnected = true;
+                                break;
+                            }
+                        }
+                        for (int i = 0; disconnected && i < clients.Length; ++i)
+                        {
+                            if (e.ipep == clients[i].ipep)
+                            {
+                                lock (clientsLock)
+                                {
+                                    clientsData[i] = new ClientData();
+                                }
+                            }
+                        }
+
+                        break;
+                    case EVENT_TYPE.EVENT_KEEPCONNECT:
+
+                        for (int i = 0; i < clients.Length; ++i)
+                        {
+                            if (clients[i].ipep == e.ipep)
+                            {
+                                lock (clientsLock)
+                                {
+                                    clientsData[i].lastContact = 0.0F;
+                                }
+                                break;
+                            }
+                        }
+
+                        break;
+                    case EVENT_TYPE.EVENT_CONNECTION:
+
+                        for (int i = 0; i < prts.Length; ++i)
+                        {
+                            if (!prts[i].isUsed)
+                            {
+                                lock (portsLock)
+                                {
+                                    ports[i].isUsed = true;
+                                    ports[i].remoteIP = e.ipep.Address;
+
+                                }
+                                lock (sendQueueLock)
+                                {
+                                    sendQueue.Enqueue(e); // TODO sent reserved port
+                                }
+                                break;
+                            }
+                        }
+                        for (int i = 0; i < clients.Length; ++i)
+                        {
+                            if (clients[i].id == 0)
+                            {
+                                lock (clientsLock)
+                                {
+                                    clientsData[i].lastContact = 0.0F;
+                                    clientsData[i].ipep = e.ipep;
+                                    clientsData[i].id = int.Parse(e.data.Substring(0, 3));
+                                    clientsData[i].name = e.data.Substring(4);
+                                }
+                            }
+                        }
+
+                        break;
+                    case EVENT_TYPE.EVENT_MESSAGE:
+                        break;
+                    case EVENT_TYPE.EVENT_UPDATE:
+                        break;
+                }
+            }
+        }
     }
 
     // This thread is responsible to rebound and broadcast data
     private void ThreadServerOutBound()
     {
+        while (true)
+        {
 
+        }
     }
 
-    //void ThreatSrvConnect()
-    //{
-    //    // Fill variables data and remote with client through the socket binded 
-    //    recv = serverSocket.ReceiveFrom(data, ref remote);
-
-    //    // Print debug message with client information
-    //    Debug.Log("Message received from {0}: " + remote.ToString());
-    //    Debug.Log(Encoding.ASCII.GetString(data, 0, recv));
-
-    //    string welcome = "Welcome to my test server";
-    //    data = Encoding.ASCII.GetBytes(welcome);
-    //    // Send message to client 
-    //    serverSocket.SendTo(data, data.Length, SocketFlags.None, remote);
-    //    // Messaging server/client loop
-
-    //    while (true)
-    //    {
-    //        // Debug.Log("Server Online");
-
-
-    //        // Clear the content of the data variable
-    //        data = new byte[1024];
-    //        recv = serverSocket.ReceiveFrom(data, ref remote);
-
-    //        Debug.Log("Recived from client " + Encoding.ASCII.GetString(data, 0, recv));
-
-    //        data = new byte[1024];
-    //        tmpMessage = "I can see you WOO";
-    //        data = Encoding.ASCII.GetBytes(tmpMessage);
-
-    //        serverSocket.SendTo(data, recv, SocketFlags.None, remote);
-    //    }
-    //    Debug.Log("Stopping server");
-    //    newsock.Close();
-    //}
-
+    // Get local IP
+    private void GetHostIP()
+    {
+        IPHostEntry entry = Dns.GetHostEntry(Dns.GetHostName());
+        for (int i = 0; i < entry.AddressList.Length; ++i)
+        {
+            if (entry.AddressList[i].AddressFamily == AddressFamily.InterNetwork)
+            {
+                host = entry.AddressList[i];
+                break;
+            }
+        }
+    }
 
 }
 
