3cbdec6f587f39746f84c02c947a2c9cccd50d13
Marc Rosell <36154632+MarcRosellH@users.noreply.github.com>
2022-12-22 20:00:34 +0100

    Update WorldUpdateClient.cs
    Co-Authored-By: Alejandro Moreno <61042882+JandroK@users.noreply.github.com>
    Co-Authored-By: Denisdrk6 <61042922+Denisdrk6@users.noreply.github.com>
    Co-Authored-By: IsmaUPC <61043099+IsmaUPC@users.noreply.github.com>

--- Handball Fu/Assets/Scripts/UDP/WorldUpdateClient.cs
+++ Handball Fu/Assets/Scripts/UDP/WorldUpdateClient.cs
@@ -170,13 +170,6 @@ public class WorldUpdateClient : MonoBehaviour
 
     public void AddWorldObjectsPendingSpawn(byte netID, byte type, int[] cosmeticsIdxs, int portID, Transform tform = null)
     {
-        for(int i = 0; i < worldObjects.Count; ++i)
-        {
-            if (worldObjects[i].netId == netID)
-            {
-                return;
-            }
-        }
         WorldObjInfo wops = new WorldObjInfo();
         wops.type = type;
         wops.isMyObject = client.GetPortIdx() == portID;
@@ -185,6 +178,13 @@ public class WorldUpdateClient : MonoBehaviour
         wops.portID = portID;
         wops.idxs = cosmeticsIdxs;
 
+        for (int i = 0; i < worldObjects.Count; ++i)
+        {
+            if (worldObjects[i].netId == wops.netID)
+            {
+                return;
+            }
+        }
         worldObjQueue.Enqueue(wops);
     }
 
